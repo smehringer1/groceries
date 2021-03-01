b83efc0ff6841ce4dd2004ebfb9b50bfda366eb9
spencer <spencermehringer@gmail.com>
2021-02-28 23:32:13 -0500

    Removed hanging userRouter in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,13 +3,11 @@ import express, {Request, Response} from 'express';
 const app = express();
 const PORT = 8000;
 
-let userRouter = require('./routes/userRoutes');
 let authRoutes = require('./routes/authRoutes');
 let protectedRouter = require('./routes/protected/protectedRoutes')
 
 app.use(express.json());
 
-app.use('/users', userRouter);
 app.use('/auth', authRoutes);
 app.use('/protected', protectedRouter)
 app.get('/', (req: Request, res: Response) => res.send('Express + TypeScript Server'));
