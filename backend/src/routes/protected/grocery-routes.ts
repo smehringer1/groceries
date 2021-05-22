892c05032839d4da1d86282645242a50553ef461
spencer <spencermehringer@gmail.com>
2021-05-21 23:15:40 -0400

    Remove unncessary file dependency

--- backend/src/routes/protected/grocery-routes.ts
+++ backend/src/routes/protected/grocery-routes.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express';
+import express, {Response} from 'express';
 import {AuthenticatedRequest, GroceryRaw, GroceryCreationResponse} from '../../utils/interfaces'
 import { GroceryItem } from '@prisma/client';
 import { createGrocery, deleteGrocery, getGroceries } from '../../services/groceries-service';
